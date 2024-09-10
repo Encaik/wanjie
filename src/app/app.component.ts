c3b5530701934ee243de1d8e82c887cdeaf0a6ff
shatong <843391622@qq.com>
2024-09-10 14:15:55 +0800

    feat(ui): 网格系统和组件注册的重构及样式调整

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { HeaderComponent } from './layouts/header/header.component';
+import { CharacterComponent } from './layouts/character/character.component';
+
+const layouts = [HeaderComponent, CharacterComponent];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NzLayoutModule],
+  imports: [RouterOutlet, NzLayoutModule, ...layouts],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
---
+++ src/app/layouts/character/character.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character',
+  standalone: true,
+  imports: [],
+  templateUrl: './character.component.html',
+  styleUrl: './character.component.less'
+})
+export class CharacterComponent {
+
+}
