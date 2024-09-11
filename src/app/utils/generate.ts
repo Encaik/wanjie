79becf7c895c73fd62b47a7eac310c90de10e1ab
shatong <843391622@qq.com>
2024-09-11 15:15:47 +0800

    refactor(generate): 使用外部数据集生成角色和环境

--- src/app/utils/generate.ts
+++ src/app/utils/generate.ts
@@ -1,31 +1,52 @@
+import SURNAMES from '../assets/data/surnames.json';
+import NAMES from '../assets/data/names.json';
+import WORLDS from '../assets/data/worlds.json';
+
+const SURNAMES_LEN = SURNAMES.length;
+const NAMES_LEN = NAMES.length;
+const WORLDS_LEN = WORLDS.length;
+
 export class Generate {
   static characters(length: number) {
     return Array.from({ length }, (_, i) => ({
-      name: `姓名${i + 1}`,
+      name: getCharacterName(),
       gender: Math.random() > 0.5 ? '男' : '女',
-      age: Math.floor(Math.random() * 100),
+      age: Math.floor(Math.random() * 20) * 2,
       ability: '特质' + Math.floor(Math.random() * 100),
     }));
   }
 
   static envs(length: number) {
-    return Array.from({ length }, (_, i) => ({
-      name: `世界${i + 1}`,
-      levelMap: {
-        0: '新手',
-        1: '初级',
-        2: '中级',
-        3: '高级',
-        4: '专家',
-        5: '王者',
-        6: '宗师',
-        7: '至尊',
-        8: '无尽',
-        9: '终极',
-        10: '传说',
-      },
-      weight: 1,
-      maxEnergy: 10000,
-    }));
+    return Array.from({ length }, (_, i) => {
+      const weight: number = Number((Math.random() * 0.5 + 0.75).toFixed(2));
+      return {
+        name: WORLDS[Math.floor(Math.random() * WORLDS_LEN)],
+        levelMap: {
+          0: '新手',
+          1: '初级',
+          2: '中级',
+          3: '高级',
+          4: '专家',
+          5: '王者',
+          6: '宗师',
+          7: '至尊',
+          8: '无尽',
+          9: '终极',
+          10: '传说',
+        },
+        weight,
+        maxEnergy: Math.floor(9000 + 1000 * Math.random() * weight),
+      };
+    });
   }
+}
+
+function getCharacterName() {
+  let name = '';
+  name += SURNAMES[Math.floor(Math.random() * SURNAMES_LEN)];
+  name += NAMES[Math.floor(Math.random() * NAMES_LEN)];
+  if (Math.random() > 0.5) {
+    name += NAMES[Math.floor(Math.random() * NAMES_LEN)];
+  }
+  return name;
 }
