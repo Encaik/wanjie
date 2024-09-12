1302c4b4d8e3af0dcff58d35ad3e64f93ff82d89
shatong <843391622@qq.com>
2024-09-12 20:27:37 +0800

    feat(vercel): 集成Vercel速度洞察以提升性能监测

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
+injectSpeedInsights();
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
