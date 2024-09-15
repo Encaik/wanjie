4ce7e5174f9b263e46b74ffc57a4ff7728c1471c
姑臧折柳曲 <843391622@qq.com>
2024-09-15 14:56:09 +0800

    test(runtime): 确保startPageGuard的单元测试正确设置依赖注入

--- src/app/guards/start-page.guard.spec.ts
+++ src/app/guards/start-page.guard.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 
+import { RuntimeService } from '../services/runtime.service';
 import { startPageGuard } from './start-page.guard';
 
 describe('startPageGuard', () => {
+  let router: jasmine.SpyObj<Router>;
+  let runtimeService: jasmine.SpyObj<RuntimeService>;
   const executeGuard: CanActivateFn = (...guardParameters) => TestBed.runInInjectionContext(() => startPageGuard(...guardParameters));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Router, useValue: jasmine.createSpyObj('Router', ['navigate']) },
+        { provide: RuntimeService, useValue: jasmine.createSpyObj('RuntimeService', ['isInit']) }
+      ]
+    });
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    runtimeService = TestBed.inject(RuntimeService) as jasmine.SpyObj<RuntimeService>;
   });
 
   it('should be created', () => {
+    expect(executeGuard).toBeTruthy();
+  });
+
+  it('should not redirect if already initialized', () => {
+    runtimeService.isInit = true;
+    expect(router.navigate).not.toHaveBeenCalled();
     expect(executeGuard).toBeTruthy();
   });
 });
