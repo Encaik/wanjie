bbd3c99f5c1c2c610b353d89dcacec562a42ee5c
shatong <843391622@qq.com>
2024-09-27 16:27:52 +0800

    feat(spec): 测试设置中注入NzModalService以支持对话框组件

--- src/app/services/battle.service.spec.ts
+++ src/app/services/battle.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BattleService } from './battle.service';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 describe('BattleService', () => {
   let service: BattleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [NzModalService]
+    });
     service = TestBed.inject(BattleService);
   });
 
