fcfd73f7f652fa3627e5199daf64c69c50ce3f02
姑臧折柳曲 <843391622@qq.com>
2024-09-28 23:35:04 +0800

    feat(env): 生产模式增强和环境文件配置

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { inject } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { environment } from 'environments/environment';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
 injectSpeedInsights();
 inject();
+
+if (environment.production) {
+  enableProdMode();
+}
+
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
